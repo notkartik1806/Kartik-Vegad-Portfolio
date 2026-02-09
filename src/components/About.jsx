2abb383874985bb6c0e88b54e31086759cbcc1af
Kartik Vegad <156411236+notkartik1806@users.noreply.github.com>
2026-02-09 11:55:11 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import { useEffect, useRef } from 'react';
 
 const About = () => {
     const downloadResume = (e) => {
-        // Button animation
+        // Add a subtle animation to the button
         const resumeBtn = e.target;
         resumeBtn.style.transform = 'scale(0.95)';
 
@@ -10,11 +10,13 @@ const About = () => {
             resumeBtn.style.transform = 'scale(1)';
         }, 150);
 
-        const pdfUrl = `${import.meta.env.BASE_URL}Resume - Kartik Vegad.pdf`;
-        window.open(pdfUrl, '_blank');
+        const link = document.createElement('a');
+        link.href = `${import.meta.env.BASE_URL}Resume - Kartik Vegad.pdf`;
+        link.download = "Kartik_Vegad_Resume.pdf";
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
     };
-};
-
 
     return (
         <section id="about" className="section">
